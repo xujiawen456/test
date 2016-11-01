77530491760161408933bb8e004ce8b31f570efb
人间四月天 <xjw12345@126.com>
2016-11-01 14:09:46 +0800

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by czy on 16/11/1.
+//  Copyright © 2016年 czy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
